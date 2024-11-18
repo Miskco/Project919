bd0689468ba816e0f61901dcc9270784facb2e12
Miskco <chekochn@gmail.com>
2024-11-18 14:20:41 +0800

    complete

--- snake/app.js
+++ snake/app.js
@@ -7,45 +7,111 @@ const row = canvas.height / unit;
 const column = canvas.width / unit;
 
 let snake = []; //array中的每个元素都是object,储存蛇身体的x和y坐标
-snake[0] = {
-  x: 80,
-  y: 0,
-};
+function createSnake() {
+  snake[0] = {
+    x: 80,
+    y: 0,
+  };
 
-snake[1] = {
-  x: 60,
-  y: 0,
-};
+  snake[1] = {
+    x: 60,
+    y: 0,
+  };
 
-snake[2] = {
-  x: 40,
-  y: 0,
-};
+  snake[2] = {
+    x: 40,
+    y: 0,
+  };
 
-snake[3] = {
-  x: 20,
-  y: 0,
-};
+  snake[3] = {
+    x: 20,
+    y: 0,
+  };
+}
+
+//果实
+class Fruit {
+  constructor() {
+    this.x = Math.floor(Math.random() * column) * unit;
+    this.y = Math.floor(Math.random() * row) * unit;
+  }
+
+  drawFruit() {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(this.x, this.y, unit, unit);
+  }
+
+  pickALocation() {
+    let overlapping = false;
+    let new_x;
+    let new_y;
+
+    function checkOverlap(new_x, new_y) {
+      for (let i = 0; i < snake.length; i++) {
+        if (new_x == snake[i].x && new_y == snake[i].y) {
+          console.log("overlapping...");
+          overlapping = true;
+          return;
+        } else {
+          overlapping = false;
+        }
+      }
+    }
+
+    do {
+      new_x = Math.floor(Math.random() * column) * unit;
+      new_y = Math.floor(Math.random() * row) * unit;
+      console.log("果实可能的新坐标: ", new_x, new_y);
+      checkOverlap(new_x, new_y);
+    } while (overlapping);
+
+    this.x = new_x;
+    this.y = new_y;
+  }
+}
+
+// 初始设定
+createSnake();
+let myFruit = new Fruit();
 
 window.addEventListener("keydown", changeDirection);
 let d = "Right";
 function changeDirection(e) {
-  if (e.key == "ArrowLeft") {
-    console.log("left");
-  } else if (e.key == "ArrowRight") {
-    console.log("Right");
-  } else if (e.key == "ArrowUp") {
-    console.log("Up");
-  } else if (e.key == "ArrowDown") {
-    console.log("Down");
+  if (e.key == "ArrowLeft" && d != "Right") {
+    d = "Left";
+  } else if (e.key == "ArrowRight" && d != "Left") {
+    d = "Right";
+  } else if (e.key == "ArrowUp" && d != "Down") {
+    d = "Up";
+  } else if (e.key == "ArrowDown" && d != "Up") {
+    d = "Down";
   }
+  // 每次按下上下左右鍵之後，在下一幀畫出來之前，任何keydown事件都不接受，預防連續按鍵導致自殺
+  windows.removeEventListener("keydown", changeDirection);
 }
 
+let highestScore = 0;
+loadHighestScore();
+let score = 0;
+document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
+
 function draw() {
+  //每次画图之前，确认蛇有没有咬到自己
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      clearInterval(myGame);
+      alert("Game Over");
+      return;
+    }
+  }
+
   //在每一次draw的时候，背景设置为黑色
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  //画出果实
+  myFruit.drawFruit();
   //画出蛇
   for (let i = 0; i < snake.length; i++) {
     if (i == 0) {
@@ -54,6 +120,19 @@ function draw() {
       ctx.fillStyle = "lightblue";
     }
     ctx.strokeStyle = "white";
+
+    if (snake[i].x >= canvas.width) {
+      snake[i].x = 0;
+    }
+    if (snake[i].x < 0) {
+      snake[i].x = canvas.width - unit;
+    }
+    if (snake[i].y >= canvas.height) {
+      snake[i].y = 0;
+    }
+    if (snake[i].y < 0) {
+      snake[i].y = canvas.height - unit;
+    }
 
     // x, y, width, height
     ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
@@ -68,9 +147,9 @@ function draw() {
   } else if (d == "Right") {
     snakeX += unit;
   } else if (d == "Up") {
-    snakeY += unit;
-  } else if (d == "Down") {
     snakeY -= unit;
+  } else if (d == "Down") {
+    snakeY += unit;
   }
 
   let newHead = {
@@ -78,9 +157,36 @@ function draw() {
     y: snakeY,
   };
 
+  //確認是否吃到果實
   //如果吃到果实
-  snake.pop();
+  //重新生成一个随机位置，画出果实,更新分数
+  if (snake[0].x == myFruit.x && snake[0].y == myFruit.y) {
+    myFruit.pickALocation();
+    //更新分數
+    score++;
+    setHighestScore(score);
+    document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+    document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
+  } else {
+    snake.pop();
+  }
   snake.unshift(newHead);
+  window.addEventListener("keydown", changeDirection);
 }
-
+//刷新
 let myGame = setInterval(draw, 100);
+//加載最高分數
+function loadHighestScore() {
+  if (localStorage.getItem("highestScore") == null) {
+    highestScore = 0;
+  } else {
+    highestScore = Number(localStorage.getItem("highestScore"));
+  }
+}
+//設置最高分數
+function setHighestScore(score) {
+  if (score > highestScore) {
+    localStorage.setItem("highestScore", score);
+    highestScore = score;
+  }
+}
